d6eba66ecf852dc0d0f830e07e07e0ef90ea5523
Jon <Jon@DESKTOP-E0PE27P>
2024-04-17 13:06:21 -0400

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Please type your name");
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("----------------------------------------------------------");
+Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself!");
+Console.WriteLine("\n");
