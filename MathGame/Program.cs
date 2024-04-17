a85012b93846c95cfa785a5f31c52cebc0f3d15f
pinksparrows <jonmaskee@hotmail.com>
2024-04-17 13:16:22 -0400

    Added gameSelected Var

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -5,3 +5,4 @@ var date = DateTime.UtcNow;
 Console.WriteLine("----------------------------------------------------------");
 Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself!");
 Console.WriteLine("\n");
+var gameSelected = Console.ReadLine();
