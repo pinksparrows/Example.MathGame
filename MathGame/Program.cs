5cc4721bd79a7e7eb57c80134b6f01e85702b6d4
pinksparrows <jonmaskee@hotmail.com>
2024-04-21 15:33:21 -0400

    Add, Sub, and Mult games now run 4 times and return score. Also working on Division method, currently in testing.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -52,23 +52,7 @@ Q - Quit the Prog");
 
 void DivisionGame(string message)
 {
-    Console.WriteLine(message);
-
-    var random = new Random();
-    int firstNumber = random.Next(1, 9);
-    int secondNumber = random.Next(1, 9);
-
-    Console.WriteLine($"{firstNumber} / {secondNumber}");
-    var result = Console.ReadLine();
-
-    if (int.Parse(result) == firstNumber / secondNumber)
-    {
-        Console.WriteLine("Correct!");
-    }
-    else
-    {
-        Console.WriteLine("Incorrect.");
-    }
+    GetDivisionNumbers();
 
 }
 
@@ -77,39 +61,66 @@ void MultiplicationGame(string message)
     Console.WriteLine(message);
 
     var random = new Random();
-    int firstNumber = random.Next(1, 9);
-    int secondNumber = random.Next(1, 9);
 
-    Console.WriteLine($"{firstNumber} * {secondNumber}");
-    var result = Console.ReadLine();
+    int firstNumber;
+    int secondNumber;
 
-    if (int.Parse(result) == firstNumber * secondNumber)
+    for (int i = 0; i < 5; i++)
     {
-        Console.WriteLine("Correct!");
-    }
-    else
-    {
-        Console.WriteLine("Incorrect.");
+
+
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} * {secondNumber}");
+        var result = Console.ReadLine();
+        var score = 0;
+
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("Correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
     }
 }
 
 void SubtractionGame(string message)
 {
     Console.WriteLine(message);
+
     var random = new Random();
-    int firstNumber = random.Next(1, 9);
-    int secondNumber = random.Next(1, 9);
 
-    Console.WriteLine($"{firstNumber} - {secondNumber}");
-    var result = Console.ReadLine();
+    int firstNumber;
+    int secondNumber;
 
-    if (int.Parse(result) == firstNumber - secondNumber)
+    for (int i = 0; i < 5; i++)
     {
-        Console.WriteLine("Correct!");
-    }
-    else
-    {
-        Console.WriteLine("Incorrect.");
+
+
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} - {secondNumber}");
+        var result = Console.ReadLine();
+        var score = 0;
+
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("Correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
     }
 }
 
@@ -118,20 +129,47 @@ void AdditionGame(string message)
     Console.WriteLine(message);
 
     var random = new Random();
-    int firstNumber = random.Next(1, 9);
-    int secondNumber = random.Next(1, 9);
 
-    Console.WriteLine($"{firstNumber} + {secondNumber}");
-    var result = Console.ReadLine();
+    int firstNumber;
+    int secondNumber;
 
-    if (int.Parse(result) == firstNumber + secondNumber)
+    for (int i = 0; i < 5; i++) 
     {
-        Console.WriteLine("Correct!");
-    } else
-    {
-        Console.WriteLine("Incorrect.");
+
+        
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+        var score = 0;
+
+        if (int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
     }
-
 }
 
 
+int[] GetDivisionNumbers()
+{
+    var random = new Random();
+    var firstNumber = random.Next(0, 99);
+    var secondNumber = random.Next(0, 99);
+
+    var result = new int[2];
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+
+    return result;
+}
