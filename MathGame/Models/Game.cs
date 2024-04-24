69736e8d26f960737ea08a6dd2a056f9f3395b2a
pinksparrows <jonmaskee@hotmail.com>
2024-04-24 18:13:00 -0400

    Created Models folder and Game class, created getter and setter.

---
+++ MathGame/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MathGame.Models;
+
+internal class Game
+{
+    private int _score;
+
+    public int Score
+    { 
+        get { return _score; } 
+        set { _score = value; }
+    }
+}
