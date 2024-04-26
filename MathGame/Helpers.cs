01907bccfcf0c849a74e44d92f83014d81083e77
pinksparrows <jonmaskee@hotmail.com>
2024-04-26 14:01:35 -0400

    Created Game.cs for MVU, changed methods top internal in Helpers.cs, and created List in Helpers as well.

--- MathGame/Helpers.cs
+++ MathGame/Helpers.cs
@@ -1,16 +1,19 @@
-﻿namespace MathGame
+﻿using MathGame.Models;
+
+namespace MathGame
 {
+    
     internal class Helpers
     {
-        static List<string> games = new();
-        internal static void GetGames()
+        internal static List<Game> games = new List<Game>();
+        internal static void PrintGames()
         {
             Console.Clear();
             Console.WriteLine("Games History:");
             Console.WriteLine("----------------------------------------------------------");
             foreach (var game in games)
             {
-                Console.WriteLine(game);
+                Console.WriteLine($"{game.Date} - {game.Type}: {game.Score}pts");
             }
             Console.WriteLine("----------------------------------------------------------\n");
             Console.WriteLine("Press any key to return to the Main Menu");
@@ -19,7 +22,12 @@
 
         internal static void AddToHistory(int gameScore, string gameType)
         {
-            games.Add($"{DateTime.Now} - {gameType}: {gameScore} pts");
+            games.Add(new Game
+            {
+                Date = DateTime.Now,
+                Score = gameScore,
+                Type = gameType
+            });
         }
 
         internal static int[] GetDivisionNumbers()
